708eec141d4c78582c3c111369f42c2d84c7b56f
laurens-in <laurens.hwm@gmail.com>
2019-05-06 12:23:42 +0200

    Schrift hinzugefügt

--- sketch.js
+++ sketch.js
@@ -3,6 +3,14 @@ let socket;
 let friend;
 let friends;
 let renderClick = true;
+let font,
+  fontsize;
+
+function preload() {
+  // Ensure the .ttf or .otf font stored in the assets directory
+  // is loaded before setup() and draw() are called
+  font = loadFont('assets/SourceSansPro-Regular.otf');
+}
 
 // send current drawing state
 function sendIt() {
@@ -40,6 +48,7 @@ function friender() {
 }
 
 function setup() {
+    fontsize = (windowWidth/10);
     colorMode(HSB, 255);
 
     // prototype object for representing current state
@@ -81,33 +90,33 @@ function setup() {
     socket.connect();
     
     createCanvas(windowWidth, windowHeight);
-    background(0, 0, 0);
+
+    textFont(font);
+    textSize(fontsize);
+    textAlign(CENTER, CENTER);
+    background(0);
 }
 
-/* function draw() {
-    if (mouseIsPressed || touched) {
-        friend.active = true;
-        friend.x = mouseX / windowWidth;
-        friend.y = mouseY / windowHeight;
-        sendIt();
-        stroke(0, 255, 200);
-        fill(0, 255, 200, 127);
-        ellipse(mouseX, mouseY, 5, 5);
-    } else {
-        friend.active = false;
-    }
-    friender();
-} */
 
 function draw() {
+    fill(255 - friend.color);
+    text('tap screen', windowWidth * 0.5, windowHeight * 0.5);
     colorMode(HSB, 255);
     if (mouseIsPressed) {
       if (renderClick) {
+          let chooser = [0, 255];
           friend.active = true;
           friend.color = random(255);
+          friend.randx = random();
+          friend.randx2 = (friend.randx + (random(0.25) - 0.5));
+          if (friend.color > 210) {
+              friend.sat = random(chooser);
+          }
+          else {
+              friend.sat = 0;
+          }
           sendIt();
-          fill(friend.color);
-          strokeWeight(130);
+          fill(friend.color, friend.sat, friend.color);
           rect(0, 0, windowWidth, windowHeight);
         }
         renderClick = false;
@@ -134,4 +143,3 @@ function draw() {
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
-
