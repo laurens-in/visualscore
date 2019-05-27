0ad0c27a2cd31cc4129139e43219fbf6484e0758
Cedric Spindler <cedric.spindler@gmail.com>
2019-05-27 11:34:44 +0200

    Upgrade polygrid

--- polygrid.js
+++ polygrid.js
@@ -1,19 +1,20 @@
+let poly = Array();
 
 function setup() {
   colorMode(HSB, 255);
   createCanvas(windowWidth, windowHeight);
   background(0, 0, 0);
+  poly.push(
+    createVector(40, 40),
+    createVector(40, 200),
+    createVector(600, 200),
+    createVector(600, 40)
+  );
 }
 
 function draw() {
   background(128);
   //   random2D();
-  poly = Array(
-    createVector(40, 40),
-    createVector(40, 200),
-    createVector(600, 200),
-    createVector(600, 40),
-  );
 
   noFill();
   beginShape();
