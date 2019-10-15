e4de0032e7577b9c757dcdba035fa921bf08acd7
Cedric Spindler <cedric.spindler@gmail.com>
2019-10-15 13:03:42 +0200

    Add basic server implementation with test client

---
+++ server/test-client.js
@@ -0,0 +1,37 @@
+const io = require('socket.io-client');
+socket = io('http://localhost:5001');
+
+// REGISTERED EVENTS:
+//
+// connect
+// friend-data
+// name-assignment
+// online-users
+// friend-list
+
+socket.on('connect', () => {
+  console.log('connection: ' + socket.connected);
+});
+
+socket.on('friend-data', data => {
+  console.log('friend-data:');
+  console.dir(data);
+});
+
+socket.on('name-assignment', data => {
+  console.log('name assigned: ' + data);
+});
+
+socket.on('online-users', count => {
+  console.log('online users: ' + count);
+});
+
+socket.on('friend-list', data => {
+  console.log('friend-list:');
+  console.dir(data);
+});
+
+socket.connect();
+
+// emit stuff
+socket.emit('friend-data', {x: 0, y: 0, name: "FriendName", active: false});
